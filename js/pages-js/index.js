2322a1657ee4e3254b6f57993b266fa89bcca904
Lautaro Bidrera <lautarobidrera@gmail.com>
2024-06-17 22:34:44 -0300

    lau

---
+++ js/pages-js/index.js
@@ -0,0 +1,49 @@
+Dolares:
+fetch("https://dolarapi.com/v1/dolares")
+.then(response => response.json())
+.then(data => console.log(data));
+Dolar oficial:
+fetch("https://dolarapi.com/v1/dolares/oficial")
+.then(response => response.json())
+.then(data => console.log(data));
+Dolar blue:
+fetch("https://dolarapi.com/v1/dolares/blue")
+.then(response => response.json())
+.then(data => console.log(data));
+Dolar bolsa:
+fetch("https://dolarapi.com/v1/dolares/bolsa")
+.then(response => response.json())
+.then(data => console.log(data));
+Dolar CCL:
+fetch("https://dolarapi.com/v1/dolares/contadoconliqui")
+.then(response => response.json())
+.then(data => console.log(data));
+Dolar tarjeta:
+fetch("https://dolarapi.com/v1/dolares/tarjeta")
+.then(response => response.json())
+.then(data => console.log(data));
+Dolar mayorista:
+fetch("https://dolarapi.com/v1/dolares/mayorista")
+.then(response => response.json())
+.then(data => console.log(data));
+Dolar cripto:
+fetch("https://dolarapi.com/v1/dolares/cripto")
+.then(response => response.json())
+.then(data => console.log(data));
+OTRAS MONEDAS:
+Euro:
+fetch("https://dolarapi.com/v1/cotizaciones/eur")
+.then(response => response.json())
+.then(data => console.log(data));
+Real brasileño:
+fetch("https://dolarapi.com/v1/cotizaciones/brl")
+.then(response => response.json())
+.then(data => console.log(data));
+Peso chileno:
+fetch("https://dolarapi.com/v1/cotizaciones/clp")
+.then(response => response.json())
+.then(data => console.log(data));
+Peso Uruguayo:
+fetch("https://dolarapi.com/v1/cotizaciones/uyu")
+.then(response => response.json())
+.then(data => console.log(data));
