93bc7c33affc1eec3aa12ff9df1559317b11fd3c
Piatti, Lorenzo <lorenzopiatti2003@gmail.com>
2024-07-19 22:25:25 -0300

    a

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 const tarjetas = [
     { src: '../img/BradPitt.jpg', nombre: 'Brad Pitt', descripcion: 'El cotizador de monedas me ha facilitado mucho la vida. Antes solía perder tiempo comparando precios en diferentes sitios, pero ahora con esta herramienta tengo toda la información al instante. ¡Muy recomendado!' },
-    { src: '../img/SuperMan.jpg', nombre: 'Super Man', descripcion: 'Como defensor de la justicia, siempre estoy volando alrededor del mundo, y tener acceso a un cotizador de monedas preciso y rápido es vital para mí. Esta herramienta me permite estar al tanto de los tipos de cambio actuales sin importar dónde me encuentre, lo cual es esencial cuando necesito intercambiar kryptonianos por dólares o euros. ¡Altamente recomendado para héroes y ciudadanos por igual!' },
+    { src: '../img/SuperMan.jpg', nombre: 'SuperMan', descripcion: 'Como defensor de la justicia, tener acceso a un cotizador de monedas preciso y rápido es vital para mí. Es esencial cuando necesito intercambiar kryptonianos por dólares o euros. ¡Altamente recomendado para héroes y ciudadanos por igual!' },
     { src: '../img/ZacEfron.jpeg', nombre: 'Zac Efron', descripcion: 'Como viajero frecuente, siempre necesito saber el tipo de cambio actual. Este cotizador es mi herramienta favorita porque es rápida y confiable. La uso todo el tiempo y nunca me ha decepcionado.' },
 ];
 
